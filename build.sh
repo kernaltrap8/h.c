b7ece122efef51d6ee4e06209f8ba6972fce48ce
JamesIsWack <aidenwingard2@gmail.com>
2023-11-13 13:38:37 -0600

    hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 mkdir $SCRIPT_DIR/bin
-gcc $SCRIPT_DIR/src/h.c -o $SCRIPT_DIR/bin/h
+gcc -s $SCRIPT_DIR/src/h.c -o $SCRIPT_DIR/bin/h
+
